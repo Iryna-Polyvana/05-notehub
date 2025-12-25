cc20e87f14e28ac00c482118fd77253b40eab1da
Iryna Polyvana <irynapolyvana@gmail.com>
2025-12-25 19:09:42 +0100

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import css from './App.css';
+import { object, string, number, date, InferType } from 'yup';
+
+export default function App() {
+    return (
+        <div className={css.app}>
+	        <header className={css.toolbar}>
+		    {/* Компонент SearchBox */}
+		    {/* Пагінація */}
+		    {/* Кнопка створення нотатки */}
+            </header>
+        </div>
+    )
+};
